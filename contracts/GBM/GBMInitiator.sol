0227034e0661983918b744bcd7839257b1cfe47e
Coderdan <coderdannn@gmail.com>
2021-07-09 21:49:24 +0800

    remove events

--- contracts/GBM/GBMInitiator.sol
+++ contracts/GBM/GBMInitiator.sol
@@ -22,7 +22,7 @@ contract GBMInitiator is IGBMInitiator {
     uint256 internal auction_incMax; // _auctionID => maximal earned incentives
     uint256 internal auction_bidMultiplier; // _auctionID => bid incentive growth multiplier
 
-    event InitiatorCreated(address _owner);
+    /* event InitiatorCreated(address _owner);
     event StartTimeSet(uint256 _auction_startTime);
     event EndTimeSet(uint256 _auction_endTime);
     event HammerTimeSet(uint256 _auction_hammerTimeDuration);
@@ -31,6 +31,7 @@ contract GBMInitiator is IGBMInitiator {
     event IncMinSet(uint256 _auction_incMin);
     event IncMaxSet(uint256 _auction_incMax);
     event BidMultiplierSet(uint256 _auction_bidMultiplier);
+    */
 
     modifier onlyOwner() {
         require(owner == msg.sender, "Initiator: Must be owner");
@@ -39,7 +40,7 @@ contract GBMInitiator is IGBMInitiator {
 
     constructor() {
         owner = msg.sender;
-        emit InitiatorCreated(msg.sender);
+        // emit InitiatorCreated(msg.sender);
     }
 
     function getOwner() external view returns (address) {
@@ -96,41 +97,41 @@ contract GBMInitiator is IGBMInitiator {
 
     function setStartTime(uint256 _auction_startTime) external onlyOwner {
         auction_startTime = _auction_startTime;
-        emit StartTimeSet(_auction_startTime);
+        // emit StartTimeSet(_auction_startTime);
     }
 
     function setEndTime(uint256 _auction_endTime) external onlyOwner {
         auction_endTime = _auction_endTime;
-        emit EndTimeSet(_auction_endTime);
+        // emit EndTimeSet(_auction_endTime);
     }
 
     function setHammerTimeDuration(uint256 _auction_hammerTimeDuration) external onlyOwner {
         auction_hammerTimeDuration = _auction_hammerTimeDuration;
-        emit HammerTimeSet(_auction_hammerTimeDuration);
+        //  emit HammerTimeSet(_auction_hammerTimeDuration);
     }
 
     function setBidDecimals(uint256 _auction_bidDecimals) external onlyOwner {
         auction_bidDecimals = _auction_bidDecimals;
-        emit BidDecimalsSet(_auction_bidDecimals);
+        // emit BidDecimalsSet(_auction_bidDecimals);
     }
 
     function setStepMin(uint256 _auction_stepMin) external onlyOwner {
         auction_stepMin = _auction_stepMin;
-        emit StepMinSet(_auction_stepMin);
+        // emit StepMinSet(_auction_stepMin);
     }
 
     function setIncMin(uint256 _auction_incMin) external onlyOwner {
         auction_incMin = _auction_incMin;
-        emit IncMinSet(_auction_incMin);
+        //  emit IncMinSet(_auction_incMin);
     }
 
     function setIncMax(uint256 _auction_incMax) external onlyOwner {
         auction_incMax = _auction_incMax;
-        emit IncMaxSet(_auction_incMax);
+        //  emit IncMaxSet(_auction_incMax);
     }
 
     function setBidMultiplier(uint256 _auction_bidMultiplier) external onlyOwner {
         auction_bidMultiplier = _auction_bidMultiplier;
-        emit BidMultiplierSet(_auction_bidMultiplier);
+        // emit BidMultiplierSet(_auction_bidMultiplier);
     }
 }
