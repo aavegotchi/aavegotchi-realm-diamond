c4705d469c5a7e3eca570dd04c902112d52385b8
Coderdan <dmathieuva@gmail.com>
2021-10-22 11:51:38 +0800

    added parcel metadata

--- README.md
+++ README.md
@@ -1,96 +1,10 @@
-# GBM_Diamond_Aavegotchi
+# Aavegotchi REALM Diamond
 
-Working repo for the GBM auction contract
+## Deployed Vars
 
-The GBM auction is the intellectual property of Perpetual Altruism Ltd. This code is provided for transparency and licensing purposes. To get a license, please get in touch with us at http://gbm.auction
+### Kovan
 
-## Latest Kovan deployment
-
-GBM: 0xC025B341fF094958179d6acdddBD86042430DE1d
-Initiator: 0x3EF3b22917D663ECE1896F98251fa44d96052e07
-
-### Setup the token contracts
-
-- Deploy the ERC-20 token contract that is gonna be used as currency and either ERC-721 or ERC-1155 token contracts to be used as NFTs to auction.
-- For Aavegotchi, GHST is used instead of deploying new ERC-20.
-
-For testing purposes, an implementation of each of those contract can be found in _src/contracts/test/ERCxxxGeneric.sol_
-
-### Setup the GBM Diamond and Facets
-
-- GBM Diamond and facets could be deployed with following deploy script.
-
-  _src/scripts/deploy.js_
-
-- Before deploy, you need to check and configure default aution parameters for GBM in deploy script.
-
-  ```
-  const ghstAddress = "0x385Eeac5cB85A38A9a07A70c73e0a3271CfB54A7";
-  const _pixelcraft = "0xD4151c984e6CF33E04FFAAF06c3374B2926Ecc64";
-  const _playerRewards = "0x27DF5C6dcd360f372e23d5e63645eC0072D0C098";
-  const _daoTreasury = "0xb208f8BB431f580CC4b216826AFfB128cd1431aB";
-
-  let startTime = Math.floor(Date.now() / 1000);
-  let endTime = Math.floor(Date.now() / 1000) + 86400;
-  let hammerTimeDuration = 300;
-  let bidDecimals = 100000;
-  let stepMin = 10000;
-  let incMax = 10000;
-  let incMin = 1000;
-  let bidMultiplier = 11120;
-  ```
-
-### Mint and transfer a token
-
-- Mint an ERC-721 or ERC-1155 token
-- Transfer it to the GBM diamond address.
-
-### Register default auction parameters for a Token Smart Contract
-
-- On the GBM smart contract, call _registerAnAuctionContract()_ with :  
-   ` _contract = the address of the smart ERC721 or ERC1155 tokens smart contract`
-
-### Register an auction for a token that the GBM contract hold
-
-- On the GBM smart contract, call _registerAnAuctionToken()_ with :  
-   `_contract = the address of the smart ERC721 or ERC1155 tokens smart contract`  
-   `_tokenId = the token ID of the token held by the GBM smart contract`  
-   ` _tokenKind = 0x73ad2146 if the token is ERC721, 0x973bb640 if the token is ERC1155`
-
-### Modify an auction already registered for a token that the GBM contract hold
-
-- On the GBM smart contract, call _modifyAnAuctionToken()_ with :  
-   `_contract = the address of the smart ERC721 or ERC1155 tokens smart contract`  
-   `_tokenId = the token ID of the token held by the GBM smart contract`  
-   ` _tokenKind = 0x73ad2146 if the token is ERC721, 0x973bb640 if the token is ERC1155`  
-   ` _useInitiator_ = the address of the initiator you previously deployed. Have the initater values set to 0 if wanting to rest the auction to default contract values`  
-   `_1155Index = Set to`false`if you want to use the default value registered for the token contract (if wanting to reset to default,use`true`)`
-  ` _rewrite = true if modifiying an existing auction, false if registering a new one`
-
-### Allowing bidding for tokens
-
-- On the GBM smart contract, call _setBiddingAllowed(token smart contract address, true)_ for allowing bidding and claiming on ALL auctions associated with a smart contract token address.
-- On the GBM smart contract, call _setBiddingAllowed(token smart contract address, false)_ for preventing bidding and claiming on ALL auctions associated with a smart contract token address.
-
-By default bidding/claiming is prevented.
-
-## Bidding and Claiming
-
-### Acquire currency and Authorize GBM
-
-- With the wallet you want to place a bid with in order to win an NFT, acquire/mint some of the ERC-20 currency
-- ERC20 Approve() the GBM smart contract as a spender with the amount you are willing to spend your bid. Recommended high value of "approve" as each subsequent bid will require full spending again after being refunded by the GBM smart contract.
-
-### Bid
-
-- On the GBM smart contract, call _Bid()_ from the wallet you want to place a bid with  
-  ` _auctionID = the _auctionID of the token you want to bid on. Use events or getAuctionID() to find it`  
-  ` _bidAmount = the amount of your bid in the ERC20 currency. If the GBM contract cannot withdraw from you, the transaction will fail`  
-  ` _highestBid = the current highest bid. If a new highest bid has been placed before you can place yours, the transaction will fail.`
-- When you are outbid, you will instantly receive your bid refund as well as your incentives.
-- You cannot bid before or after the auction start/end.
-- If bidding less than HammerTimeDuration seconds before the end of the auction, the auction will move it's end to be in HammerTimeDuration seconds.
-
-### Claim
-
-Once an auction as ended, anyone (including the winner) can call _claim(auctionID)_ to attribute the tokens to the highest bidder at the end of the auction and distribute the proceeds of the auction to the GBM contract owner.
+DiamondLoupeFacet deployed: 0xA978055F3116b4d2B48104deA0662F2aDF44b02B
+OwnershipFacet deployed: 0xA978055F3116b4d2B48104deA0662F2aDF44b02B
+ERC721Facet deployed: 0xA978055F3116b4d2B48104deA0662F2aDF44b02B
+RealmFacet deployed: 0xA978055F3116b4d2B48104deA0662F2aDF44b02B
---
+++ test/realmTestAuction1.ts
@@ -0,0 +1,145 @@
+import { impersonate } from "../scripts/helperFunctions";
+import { RealmFacet, ERC721Facet } from "../typechain";
+import { expect } from "chai";
+import { network } from "hardhat";
+import { ethers } from "hardhat";
+import { MintParcelInput } from "../types";
+
+const { deployDiamond } = require("../scripts/deploy.ts");
+
+const testAddress = "0xBC67F26c2b87e16e304218459D2BB60Dac5C80bC";
+const testAddress2 = "0xC99DF6B7A5130Dce61bA98614A2457DAA8d92d1c";
+let diamondAddress;
+let realmFacet: RealmFacet;
+let erc721Facet: ERC721Facet;
+let accounts;
+
+describe("Realm tests", async function () {
+  before(async function () {
+    this.timeout(20000000);
+    diamondAddress = await deployDiamond();
+    accounts = await ethers.getSigners();
+
+    realmFacet = (await ethers.getContractAt(
+      "RealmFacet",
+      diamondAddress
+    )) as RealmFacet;
+    erc721Facet = (await ethers.getContractAt(
+      "ERC721Facet",
+      diamondAddress
+    )) as ERC721Facet;
+  });
+
+  it("Token symbol should be REALM", async function () {
+    const symbol = await erc721Facet.symbol();
+    expect(symbol).to.equal("REALM");
+  });
+  it("Token name should be Gotchiverse REALM Parcel", async function () {
+    const name = await erc721Facet.name();
+    expect(name).to.equal("Gotchiverse REALM Parcel");
+  });
+  it("Token metadata url should be https://aavegotchi.com/metadata/realm/0", async function () {
+    const uri = await erc721Facet.tokenURI("0");
+    expect(uri).to.equal("https://aavegotchi.com/metadata/realm/0");
+  });
+
+  it("Check that tokens are being minted", async function () {
+    const parcelsTest1: MintParcelInput[] = [];
+    const parcelsTest2: MintParcelInput[] = [];
+    for (let i = 0; i < 4; i++) {
+      const size = Math.floor(Math.random() * 5);
+      const boostFomo = Math.floor(Math.random() * 4);
+      const boostFud = Math.floor(Math.random() * 4);
+      const boostKek = Math.floor(Math.random() * 4);
+      const boostAlpha = Math.floor(Math.random() * 4);
+      parcelsTest1.push({
+        coordinateX: i,
+        coordinateY: i,
+        parcelId: i,
+        size,
+        fomoBoost: boostFomo,
+        fudBoost: boostFud,
+        kekBoost: boostKek,
+        alphaBoost: boostAlpha,
+      });
+    }
+    for (let i = 0; i < 6; i++) {
+      const size = Math.floor(Math.random() * 5);
+      const boostFomo = Math.floor(Math.random() * 4);
+      const boostFud = Math.floor(Math.random() * 4);
+      const boostKek = Math.floor(Math.random() * 4);
+      const boostAlpha = Math.floor(Math.random() * 4);
+      parcelsTest2.push({
+        coordinateX: i,
+        coordinateY: i,
+        parcelId: i,
+        size,
+        fomoBoost: boostFomo,
+        fudBoost: boostFud,
+        kekBoost: boostKek,
+        alphaBoost: boostAlpha,
+      });
+    }
+    await realmFacet.mintParcels(testAddress, [5, 200, 33, 2], parcelsTest1);
+    await realmFacet.mintParcels(
+      testAddress2,
+      [555, 7, 44, 0, 800, 6],
+      parcelsTest2
+    );
+    await expect(
+      realmFacet.mintParcels(testAddress, [0], [parcelsTest1[0]])
+    ).to.be.revertedWith("ERC721: tokenId already minted");
+    const balanceTest1 = await erc721Facet.balanceOf(testAddress);
+    expect(balanceTest1).to.equal(parcelsTest1.length);
+    const balanceTest2 = await erc721Facet.balanceOf(testAddress2);
+    expect(balanceTest2).to.equal(parcelsTest2.length);
+    const totalSupply = await erc721Facet.totalSupply();
+    expect(totalSupply).to.equal(parcelsTest1.length + parcelsTest2.length);
+  });
+  it("Can transfer tokens", async function () {
+    erc721Facet = await impersonate(testAddress, erc721Facet, ethers, network);
+    const balancePreSender = await erc721Facet.balanceOf(testAddress);
+    const balancePreReceiver = await erc721Facet.balanceOf(testAddress2);
+    await erc721Facet["safeTransferFrom(address,address,uint256)"](
+      testAddress,
+      testAddress2,
+      33
+    );
+    const balancePostSender = await erc721Facet.balanceOf(testAddress);
+    const balancePostReceiver = await erc721Facet.balanceOf(testAddress2);
+    expect(balancePostSender).to.equal(balancePreSender.sub(1));
+    expect(balancePostReceiver).to.equal(balancePreReceiver.add(1));
+  });
+  it("Only owner can transfer", async function () {
+    erc721Facet = await impersonate(testAddress, erc721Facet, ethers, network);
+    await expect(
+      erc721Facet["safeTransferFrom(address,address,uint256)"](
+        testAddress,
+        testAddress2,
+        33
+      )
+    ).to.be.revertedWith("LibERC721: Not owner or approved to transfer");
+  });
+
+  it("Can batch transfer", async function () {
+    const balancePreSender = await erc721Facet.balanceOf(testAddress);
+    const balancePreReceiver = await erc721Facet.balanceOf(testAddress2);
+    erc721Facet = await impersonate(testAddress, erc721Facet, ethers, network);
+    await erc721Facet.safeBatchTransfer(
+      testAddress,
+      testAddress2,
+      [5, 200],
+      []
+    );
+    const balancePostSender = await erc721Facet.balanceOf(testAddress);
+    const balancePostReceiver = await erc721Facet.balanceOf(testAddress2);
+    expect(balancePostSender).to.equal(balancePreSender.sub(2));
+    expect(balancePostReceiver).to.equal(balancePreReceiver.add(2));
+  });
+  it("Cannot batch transfer tokens not owned", async function () {
+    erc721Facet = await impersonate(testAddress, erc721Facet, ethers, network);
+    await expect(
+      erc721Facet.safeBatchTransfer(testAddress, testAddress2, [5, 200], [])
+    ).to.be.revertedWith("LibERC721: Not owner or approved to transfer");
+  });
+});
