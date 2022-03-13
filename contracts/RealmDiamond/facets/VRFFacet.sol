d562faff5073100f99c5f836d4cc8d32a79a3916
Patrick <pakim249@ischool.berkeley.edu>
2022-03-13 11:29:40 -0700

    Fix comma error

--- contracts/RealmDiamond/facets/VRFFacet.sol
+++ contracts/RealmDiamond/facets/VRFFacet.sol
@@ -16,7 +16,7 @@ contract VRFFacet is Modifiers {
   }
 
   // TODO: REMOVE THIS FUNCTION IN PRODUCTION
-  function testRawFulfillRandomWords(uint256 tokenId, uint256[] memory randomWords uint256 round) external {
+  function testRawFulfillRandomWords(uint256 tokenId, uint256[] memory randomWords, uint256 round) external {
     LibRealm.updateRemainingAlchemica(tokenId, randomWords, round);
   }
 
