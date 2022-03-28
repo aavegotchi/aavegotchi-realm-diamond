99abed58ae4d72c4c2141c8dbe8ea10c1f19ac0b
Coderdan <dmathieuva@gmail.com>
2022-03-28 13:33:04 +0800

    remove test functions

--- contracts/RealmDiamond/facets/AlchemicaFacet.sol
+++ contracts/RealmDiamond/facets/AlchemicaFacet.sol
@@ -139,49 +139,49 @@ contract AlchemicaFacet is Modifiers {
   }
 
   /// @dev This function will be removed in production.
-  function testingStartSurveying(uint256 _realmId) external onlyParcelOwner(_realmId) {
-    require(s.parcels[_realmId].currentRound <= s.surveyingRound, "AlchemicaFacet: Round not released");
-    require(s.parcels[_realmId].altarId > 0, "AlchemicaFacet: Must equip Altar");
-    s.parcels[_realmId].currentRound++;
-    uint256[] memory alchemicas = new uint256[](4);
-    for (uint256 i; i < 4; i++) {
-      alchemicas[i] = uint256(keccak256(abi.encodePacked(msg.sender, uint256(1))));
-    }
+  // function testingStartSurveying(uint256 _realmId) external onlyParcelOwner(_realmId) {
+  //   require(s.parcels[_realmId].currentRound <= s.surveyingRound, "AlchemicaFacet: Round not released");
+  //   require(s.parcels[_realmId].altarId > 0, "AlchemicaFacet: Must equip Altar");
+  //   s.parcels[_realmId].currentRound++;
+  //   uint256[] memory alchemicas = new uint256[](4);
+  //   for (uint256 i; i < 4; i++) {
+  //     alchemicas[i] = uint256(keccak256(abi.encodePacked(msg.sender, uint256(1))));
+  //   }
 
-    LibRealm.updateRemainingAlchemica(_realmId, alchemicas, s.parcels[_realmId].currentRound - 1);
-  }
-
-  /// @dev This function will be removed in production.
-  function testingMintParcel(
-    address _to,
-    uint256[] calldata _tokenIds,
-    RealmFacet.MintParcelInput[] memory _metadata
-  ) external {
-    for (uint256 index = 0; index < _tokenIds.length; index++) {
-      require(s.tokenIds.length < 420069, "AlchemicaFacet: Cannot mint more than 420,069 parcels");
-      uint256 tokenId = _tokenIds[index];
-      RealmFacet.MintParcelInput memory metadata = _metadata[index];
-      require(_tokenIds.length == _metadata.length, "Inputs must be same length");
-
-      Parcel storage parcel = s.parcels[tokenId];
-      parcel.coordinateX = metadata.coordinateX;
-      parcel.coordinateY = metadata.coordinateY;
-      parcel.parcelId = metadata.parcelId;
-      parcel.size = metadata.size;
-      parcel.district = metadata.district;
-      parcel.parcelAddress = metadata.parcelAddress;
-
-      parcel.alchemicaBoost = metadata.boost;
-
-      LibERC721.safeMint(_to, tokenId);
-    }
-  }
+  //   LibRealm.updateRemainingAlchemica(_realmId, alchemicas, s.parcels[_realmId].currentRound - 1);
+  // }
 
   /// @dev This function will be removed in production.
-  function testingAlchemicaFaucet(uint256 _alchemicaType, uint256 _amount) external {
-    IERC20Mintable alchemica = IERC20Mintable(s.alchemicaAddresses[_alchemicaType]);
-    alchemica.mint(msg.sender, _amount);
-  }
+  // function testingMintParcel(
+  //   address _to,
+  //   uint256[] calldata _tokenIds,
+  //   RealmFacet.MintParcelInput[] memory _metadata
+  // ) external {
+  //   for (uint256 index = 0; index < _tokenIds.length; index++) {
+  //     require(s.tokenIds.length < 420069, "AlchemicaFacet: Cannot mint more than 420,069 parcels");
+  //     uint256 tokenId = _tokenIds[index];
+  //     RealmFacet.MintParcelInput memory metadata = _metadata[index];
+  //     require(_tokenIds.length == _metadata.length, "Inputs must be same length");
+
+  //     Parcel storage parcel = s.parcels[tokenId];
+  //     parcel.coordinateX = metadata.coordinateX;
+  //     parcel.coordinateY = metadata.coordinateY;
+  //     parcel.parcelId = metadata.parcelId;
+  //     parcel.size = metadata.size;
+  //     parcel.district = metadata.district;
+  //     parcel.parcelAddress = metadata.parcelAddress;
+
+  //     parcel.alchemicaBoost = metadata.boost;
+
+  //     LibERC721.safeMint(_to, tokenId);
+  //   }
+  // }
+
+  // /// @dev This function will be removed in production.
+  // function testingAlchemicaFaucet(uint256 _alchemicaType, uint256 _amount) external {
+  //   IERC20Mintable alchemica = IERC20Mintable(s.alchemicaAddresses[_alchemicaType]);
+  //   alchemica.mint(msg.sender, _amount);
+  // }
 
   /// @notice Query the available alchemica in a parcel
   /// @param _realmId identifier of parcel to query
