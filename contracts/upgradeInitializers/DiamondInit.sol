07c557f11f5ee966af654b5c5b6318b672e59a48
Coderdan <dmathieuva@gmail.com>
2022-03-30 20:40:02 +0800

    Update DiamondInit.sol
    add ierc1155 interfaceId

--- contracts/upgradeInitializers/DiamondInit.sol
+++ contracts/upgradeInitializers/DiamondInit.sol
@@ -13,6 +13,7 @@ import {IDiamondLoupe} from "../interfaces/IDiamondLoupe.sol";
 import {IDiamondCut} from "../interfaces/IDiamondCut.sol";
 import {IERC173} from "../interfaces/IERC173.sol";
 import {IERC165} from "../interfaces/IERC165.sol";
+import {IERC1155} from "../interfaces/IERC1155.sol";
 import {AppStorage} from "../libraries/AppStorage.sol";
 
 contract DiamondInit {
@@ -26,5 +27,6 @@ contract DiamondInit {
     ds.supportedInterfaces[type(IDiamondCut).interfaceId] = true;
     ds.supportedInterfaces[type(IDiamondLoupe).interfaceId] = true;
     ds.supportedInterfaces[type(IERC173).interfaceId] = true;
+    ds.supportedInterfaces[type(IERC1155).interfaceId] = true;
   }
 }
