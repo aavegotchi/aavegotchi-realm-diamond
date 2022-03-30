7d323efec640898e909e817cdb8105b6cbf60382
Coderdan <dmathieuva@gmail.com>
2022-03-30 21:49:54 +0800

    Update addInstallationTypes.ts

--- scripts/installation/addInstallationTypes.ts
+++ scripts/installation/addInstallationTypes.ts
@@ -1,8 +1,45 @@
+import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
 import { installationTypes } from "../../data/installations/installationTypes";
 import { InstallationAdminFacet, InstallationFacet } from "../../typechain";
+import { InstallationTypeInput, InstallationTypeOutput } from "../../types";
 
-import { outputInstallation } from "../realm/realmHelpers";
+function outputInstallation(
+  installation: InstallationTypeInput
+): InstallationTypeOutput {
+  if (installation.width > 64) throw new Error("Width too much");
+  if (installation.height > 64) throw new Error("Height too much");
+
+  const alchemica = installation.alchemicaCost.map((val) =>
+    ethers.utils.parseEther(val.toString())
+  );
+
+  let output: InstallationTypeOutput = {
+    deprecated: installation.deprecated,
+    installationType: installation.installationType,
+    level: installation.level,
+    width: installation.width,
+    height: installation.height,
+    alchemicaType: installation.alchemicaType,
+    alchemicaCost: [
+      BigNumber.from(alchemica[0]),
+      BigNumber.from(alchemica[1]),
+      BigNumber.from(alchemica[2]),
+      BigNumber.from(alchemica[3]),
+    ],
+    harvestRate: ethers.utils.parseEther(installation.harvestRate.toString()),
+    capacity: ethers.utils.parseEther(installation.capacity.toString()),
+    spillRadius: ethers.utils.parseEther(installation.spillRadius.toString()),
+    spillRate: ethers.utils.parseEther(installation.spillRate.toString()),
+    upgradeQueueBoost: installation.upgradeQueueBoost,
+    craftTime: installation.craftTime,
+    nextLevelId: installation.nextLevelId,
+    prerequisites: installation.prerequisites,
+    name: installation.name,
+  };
+
+  return output;
+}
 
 export async function setAddresses() {
   const diamondAddress = "0x19f870bD94A34b3adAa9CaA439d333DA18d6812A";
