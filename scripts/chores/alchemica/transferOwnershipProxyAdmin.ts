201750a9d1603e69017e0f0f9648ede6d7256618
Patrick <pakim249@ischool.berkeley.edu>
2022-04-20 00:45:34 -0700

    Add to transferring ownership and added lots of logs

--- scripts/chores/alchemica/transferOwnershipProxyAdmin.ts
+++ scripts/chores/alchemica/transferOwnershipProxyAdmin.ts
@@ -19,33 +19,28 @@ import {
 
 export async function setAddresses() {
   const accounts: Signer[] = await ethers.getSigners();
-  const deployer = accounts[0];
+  let deployer = accounts[0];
 
+  const currentProxyAdminOwner = "0x94cb5C277FCC64C274Bd30847f0821077B231022";
   const currentProxyAdmin = "0xB549125b4A2F3c1B4319b798EcDC72b04315dF2D";
-  const multisig = "0xa370f2ADd2A9Fba8759147995d6A0641F8d7C119"; //multisig
+  const pixelcraftMultisig = "0x35FE3dF776474a7B24B3B1EC6e745a830FdAd351"; //multisig
 
+  // Old Proxy Admin
   const ProxyAdmin = await ethers.getContractFactory("ProxyAdmin");
   let oldProxyAdmin = (await (
     await ethers.getContractAt("ProxyAdmin", currentProxyAdmin)
   ).connect(deployer)) as ProxyAdmin;
   console.log("Old Proxy Admin Address: ", oldProxyAdmin.address);
   console.log("Old Proxy Admin Owner: ", await oldProxyAdmin.owner());
+
+  // New Proxy Admin
   let newProxyAdmin: ProxyAdmin = (await ProxyAdmin.deploy()) as ProxyAdmin;
   await newProxyAdmin.deployed();
-  await newProxyAdmin.transferOwnership(multisig);
+  await newProxyAdmin.transferOwnership(pixelcraftMultisig);
   console.log("New Proxy Admin Address: ", newProxyAdmin.address);
   console.log("New Proxy Admin Owner: ", await newProxyAdmin.owner());
 
   const testing = ["hardhat"].includes(network.name);
-
-  if (testing) {
-    oldProxyAdmin = await impersonate(
-      await oldProxyAdmin.owner(),
-      oldProxyAdmin,
-      ethers,
-      network
-    );
-  }
 
   const fud = await ethers.getContractAt("AlchemicaToken", FUD_ADDRESS);
   const fomo = await ethers.getContractAt("AlchemicaToken", FOMO_ADDRESS);
@@ -59,6 +54,30 @@ export async function setAddresses() {
     "AlchemicaVesting",
     ECOSYSTEM_VESTING_ADDRESS
   );
+
+  if (testing) {
+    oldProxyAdmin = await impersonate(
+      currentProxyAdminOwner,
+      oldProxyAdmin,
+      ethers,
+      network
+    );
+    deployer = oldProxyAdmin.signer;
+  }
+
+  console.log("FUD Current Owner: ", await fud.owner());
+  console.log("FOMO Current Owner: ", await fomo.owner());
+  console.log("Alpha Current Owner: ", await alpha.owner());
+  console.log("Kek Current Owner: ", await kek.owner());
+  console.log(
+    "Current Gameplay Vesting Owner: ",
+    await gameplayVesting.owner()
+  );
+  console.log(
+    "Current Ecosystem Vesting Owner: ",
+    await ecosystemVesting.owner()
+  );
+  /********** Proxy Upgradeability Transfers ***********/
 
   // ALCHEMICA PROXY ADMIN TRANSFERS
 
@@ -93,6 +112,20 @@ export async function setAddresses() {
   );
   await tx.wait();
 
+  /*********** Contract ownership transfers ************/
+
+  // TRANSFERRING OWNERSHIP OF CONTRACTS
+  tx = await gameplayVesting
+    .connect(deployer)
+    .transferOwnership(pixelcraftMultisig);
+  await tx.wait();
+  tx = await ecosystemVesting
+    .connect(deployer)
+    .transferOwnership(pixelcraftMultisig); // TODO: REPLACE THIS ADDRESS WITH DESIRED OWNER
+  await tx.wait();
+
+  /*** LOGS ***/
+
   console.log(
     "FUD new Proxy Admin: ",
     await newProxyAdmin.getProxyAdmin(FUD_ADDRESS)
@@ -117,6 +150,12 @@ export async function setAddresses() {
     "ECOSYSTEM Vesting new Proxy Admin: ",
     await newProxyAdmin.getProxyAdmin(ECOSYSTEM_VESTING_ADDRESS)
   );
+  console.log("FUD new owner: ", await fud.owner());
+  console.log("FOMO new owner: ", await fomo.owner());
+  console.log("ALPHA new owner: ", await alpha.owner());
+  console.log("KEK new owner: ", await kek.owner());
+  console.log("GAMEPLAY Vesting new owner: ", await gameplayVesting.owner());
+  console.log("ECOSYSTEM Vesting new owner: ", await ecosystemVesting.owner());
 }
 
 // We recommend this pattern to be able to use async/await everywhere
