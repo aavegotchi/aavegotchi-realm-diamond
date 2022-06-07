0864ff1554014af0bf55ecb708bd4eb0cb8c28e4
Patrick <pakim249@ischool.berkeley.edu>
2022-06-07 18:36:00 +0200

    Add types

--- types.ts
+++ types.ts
@@ -110,7 +110,23 @@ export interface TileTypeInput {
 }
 
 export type Level = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-export type Width = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+export type Width =
+  | 1
+  | 2
+  | 3
+  | 4
+  | 5
+  | 6
+  | 7
+  | 8
+  | 9
+  | 10
+  | 11
+  | 12
+  | 13
+  | 14
+  | 15
+  | 16;
 export type Height = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 export interface InstallationTypeOutput {
   deprecated: boolean;
