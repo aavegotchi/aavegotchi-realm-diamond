39090e8430592cdedf46b1781079147abf8bfeff
Patrick <pakim249@ischool.berkeley.edu>
2022-06-17 17:49:15 +0200

    update upgrade script

--- scripts/realm/upgrades/upgrade-fixStartGrid.ts
+++ scripts/realm/upgrades/upgrade-fixStartGrid.ts
@@ -15,6 +15,8 @@ export async function upgrade() {
       addSelectors: [
         "function fixGridStartPositions(uint256[] memory _parcelIds,uint256[] memory _x,uint256[] memory _y,bool _isTile,bool _isTrue) external",
         "function isGridStartPosition(uint256 _parcelId,uint256 _x,uint256 _y,bool _isTile) external view returns (bool)",
+        "function moveInstallation(uint256 _realmId, uint256 _installationId, uint256 _x0, uint256 _y0, uint256 _x1, uint256 _y1) external",
+        "function moveTile(uint256 _realmId, uint256 _tileId, uint256 _x0, uint256 _y0, uint256 _x1, uint256 _y1) external",
       ],
       removeSelectors: [
         "function fixAltarLevel(uint256[] memory _parcelIds) external",
