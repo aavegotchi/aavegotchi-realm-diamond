e70f01fd66e84c91662d44d38ae09fee8b4277c6
Patrick <pakim249@ischool.berkeley.edu>
2022-06-22 20:02:27 +0200

    Rework upgrading

--- test/foundry/test.t.sol
+++ test/foundry/test.t.sol
@@ -3,18 +3,60 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
 import {InstallationFacet} from "@installation/facets/InstallationFacet.sol";
+import {RealmFacet} from "@realm/facets/RealmFacet.sol";
 import {TestConstants as C} from "@test/constants.t.sol";
 import {IDiamondCut} from "@interfaces/IDiamondCut.sol";
 import {console2} from "forge-std/console2.sol";
 import {TestUpgrades} from "@test/upgrade.t.sol";
 
 contract TestFoundryDiamond is Test, TestUpgrades {
-  IDiamondCut diamondCutFacet;
   InstallationFacet installationFacet;
+  RealmFacet realmFacet;
 
   function setUp() public {
+    deployFacets();
+    cutInstallation();
+    cutRealm();
     installationFacet = InstallationFacet(C.INSTALLATION_DIAMOND_ADDRESS_MATIC);
-    replaceInstallationFacetSelectors(C.INSTALLATION_DIAMOND_ADDRESS_MATIC, true);
+    realmFacet = RealmFacet(C.REALM_DIAMOND_ADDRESS_MATIC);
+  }
+
+  function deployFacets() internal {
+    console2.log("Deploying facets");
+    installationFacet = new InstallationFacet();
+    console2.log("New installation facet:");
+    console2.log(address(installationFacet));
+    realmFacet = new RealmFacet();
+    console2.log("New realm facet:");
+    console2.log(address(realmFacet));
+  }
+
+  function cutInstallation() internal {
+    console2.log("Cutting Installation Diamond");
+    IDiamondCut.FacetCut[] memory installationCuts = new IDiamondCut.FacetCut[](1);
+    installationCuts[0] = getReplaceFacetSelectorCutFromExistingSelector(
+      C.INSTALLATION_DIAMOND_ADDRESS_MATIC,
+      address(installationFacet),
+      InstallationFacet.getInstallationType.selector
+    );
+    logFunctionSelectors(installationCuts);
+
+    vm.prank(getDiamondOwner(C.INSTALLATION_DIAMOND_ADDRESS_MATIC));
+    IDiamondCut(C.INSTALLATION_DIAMOND_ADDRESS_MATIC).diamondCut(installationCuts, address(0), "");
+  }
+
+  function cutRealm() internal {
+    console2.log("Cutting Realm Diamond");
+    IDiamondCut.FacetCut[] memory realmCuts = new IDiamondCut.FacetCut[](1);
+    realmCuts[0] = getReplaceFacetSelectorCutFromExistingSelector(
+      C.REALM_DIAMOND_ADDRESS_MATIC,
+      address(realmFacet),
+      RealmFacet.equipInstallation.selector
+    );
+    logFunctionSelectors(realmCuts);
+
+    vm.prank(getDiamondOwner(C.REALM_DIAMOND_ADDRESS_MATIC));
+    IDiamondCut(C.REALM_DIAMOND_ADDRESS_MATIC).diamondCut(realmCuts, address(0), "");
   }
 
   function test1() public {
--- test/foundry/upgrade.t.sol
+++ test/foundry/upgrade.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
 import {InstallationFacet} from "@installation/facets/InstallationFacet.sol";
+import {RealmFacet} from "@realm/facets/RealmFacet.sol";
 import {TestConstants as C} from "@test/constants.t.sol";
 import {IDiamondCut} from "@interfaces/IDiamondCut.sol";
 import {IDiamondLoupe} from "@interfaces/IDiamondLoupe.sol";
@@ -10,6 +11,31 @@ import {Ownable} from "@interfaces/Ownable.sol";
 import {console2} from "forge-std/console2.sol";
 
 contract TestUpgrades is Test {
+  function logFunctionSelectors(IDiamondCut.FacetCut[] memory cuts) internal {
+    for (uint256 i; i < cuts.length; i++) {
+      if (cuts[i].action == IDiamondCut.FacetCutAction.Replace) {
+        console2.log("Replace Facet Selectors:");
+        for (uint256 j; j < cuts[i].functionSelectors.length; j++) {
+          console2.logBytes4(cuts[i].functionSelectors[j]);
+        }
+      } else if (cuts[i].action == IDiamondCut.FacetCutAction.Add) {
+        console2.log("Add Facet Selectors:");
+        for (uint256 j; j < cuts[i].functionSelectors.length; j++) {
+          console2.logBytes4(cuts[i].functionSelectors[j]);
+        }
+      } else {
+        console2.log("Remove Facet Selectors:");
+        for (uint256 j; j < cuts[i].functionSelectors.length; j++) {
+          console2.logBytes4(cuts[i].functionSelectors[j]);
+        }
+      }
+    }
+  }
+
+  function getDiamondOwner(address diamond) internal view returns (address) {
+    return Ownable(diamond).owner();
+  }
+
   /// @dev Intended to be used to get the old facet address for a specific facet to replace all the functions with the ones from a new facet
   function getFacetAddress(address diamond, bytes4 functionSelector) internal view returns (address) {
     return IDiamondLoupe(diamond).facetAddress(functionSelector);
@@ -35,58 +61,15 @@ contract TestUpgrades is Test {
     cut = IDiamondCut.FacetCut({facetAddress: facetAddress, action: IDiamondCut.FacetCutAction.Replace, functionSelectors: functionSelectors});
   }
 
+  function getReplaceFacetSelectorCutFromExistingSelector(
+    address diamond,
+    address facetAddress,
+    bytes4 existingSelector
+  ) internal view returns (IDiamondCut.FacetCut memory cut) {
+    cut = getReplaceFacetSelectorCut(facetAddress, getFacetSelectorsFromExistingSelector(diamond, existingSelector));
+  }
+
   function getRemoveFacetSelectorCut(bytes4[] memory functionSelectors) internal pure returns (IDiamondCut.FacetCut memory cut) {
     cut = IDiamondCut.FacetCut({facetAddress: address(0), action: IDiamondCut.FacetCutAction.Remove, functionSelectors: functionSelectors});
-  }
-
-  function replaceInstallationFacetSelectors(address diamond, bool prank) internal returns (address) {
-    address owner = Ownable(diamond).owner();
-    InstallationFacet installationFacet = new InstallationFacet();
-
-    IDiamondCut.FacetCut[] memory cut = new IDiamondCut.FacetCut[](1);
-    cut[0] = getReplaceFacetSelectorCut(
-      address(installationFacet),
-      getFacetSelectorsFromExistingSelector(diamond, installationFacet.installationsBalances.selector)
-    );
-
-    if (prank) vm.prank(owner);
-    IDiamondCut(diamond).diamondCut(cut, address(0), "");
-    return address(installationFacet);
-  }
-
-  function addInstallationFacetSelectors(
-    address diamond,
-    bytes4[] memory functionSelectors,
-    bool replace,
-    bool prank
-  ) internal returns (address) {
-    address owner = Ownable(diamond).owner();
-    InstallationFacet installationFacet;
-
-    if (replace) {
-      installationFacet = InstallationFacet(replaceInstallationFacetSelectors(diamond, prank));
-    } else {
-      installationFacet = new InstallationFacet();
-    }
-
-    IDiamondCut.FacetCut[] memory cut = new IDiamondCut.FacetCut[](1);
-    cut[0] = getAddFacetSelectorCut(address(installationFacet), functionSelectors);
-
-    if (prank) vm.prank(owner);
-    IDiamondCut(diamond).diamondCut(cut, address(0), "");
-    return address(installationFacet);
-  }
-
-  function removeInstallationFacetSelectors(
-    address diamond,
-    bytes4[] memory functionSelectors,
-    bool prank
-  ) internal {
-    address owner = Ownable(diamond).owner();
-    InstallationFacet installationFacet;
-    IDiamondCut.FacetCut[] memory cut = new IDiamondCut.FacetCut[](1);
-    cut[0] = getRemoveFacetSelectorCut(functionSelectors);
-    if (prank) vm.prank(owner);
-    IDiamondCut(diamond).diamondCut(cut, address(0), "");
   }
 }
