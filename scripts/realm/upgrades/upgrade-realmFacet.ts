2933a2feb559308565f279cf657c8bdd76389161
Coderdan <dmathieuva@gmail.com>
2022-06-28 18:01:43 +0800

    Create upgrade-realmFacet.ts

---
+++ scripts/realm/upgrades/upgrade-realmFacet.ts
@@ -0,0 +1,50 @@
+import { run, ethers, network } from "hardhat";
+import {
+  maticDiamondAddress,
+  mumbaiRealmDiamondAddress,
+} from "../../../constants";
+import {
+  convertFacetAndSelectorsToString,
+  DeployUpgradeTaskArgs,
+  FacetsAndAddSelectors,
+} from "../../../tasks/deployUpgrade";
+
+export async function upgrade(diamondAddress: string) {
+  const diamondUpgrader = "0x94cb5C277FCC64C274Bd30847f0821077B231022";
+
+  const facets: FacetsAndAddSelectors[] = [
+    {
+      facetName: "RealmFacet",
+      addSelectors: [],
+      removeSelectors: [],
+    },
+  ];
+
+  if (network.name === "mumbai") {
+    diamondAddress = mumbaiRealmDiamondAddress;
+  }
+
+  const joined = convertFacetAndSelectorsToString(facets);
+
+  const args: DeployUpgradeTaskArgs = {
+    diamondUpgrader: diamondUpgrader,
+    diamondAddress: diamondAddress,
+    facetsAndAddSelectors: joined,
+    useLedger: false,
+    useMultisig: false,
+    initAddress: ethers.constants.AddressZero,
+    initCalldata: "0x",
+  };
+
+  await run("deployUpgrade", args);
+}
+
+if (require.main === module) {
+  upgrade(maticDiamondAddress)
+    .then(() => process.exit(0))
+    // .then(() => console.log('upgrade completed') /* process.exit(0) */)
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
