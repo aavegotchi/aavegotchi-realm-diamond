daed363948a03c559001c6daf33a1faf51652bb2
Coderdan <dmathieuva@gmail.com>
2022-07-04 13:24:04 +0800

    interact deploy script

--- scripts/realm/upgrades/upgrade-Channeling:interact.ts
+++ scripts/realm/upgrades/upgrade-Channeling:interact.ts
@@ -1,14 +1,11 @@
 import { run, ethers } from "hardhat";
-import {
-  maticInstallationDiamondAddress,
-  maticRealmDiamondAddress,
-} from "../../../constants";
+import { varsForNetwork } from "../../../constants";
+
 import {
   convertFacetAndSelectorsToString,
   DeployUpgradeTaskArgs,
   FacetsAndAddSelectors,
 } from "../../../tasks/deployUpgrade";
-import { OwnershipFacet } from "../../../typechain";
 
 export async function upgrade() {
   const diamondUpgrader = "0xa370f2ADd2A9Fba8759147995d6A0641F8d7C119";
@@ -16,25 +13,18 @@ export async function upgrade() {
   const facets: FacetsAndAddSelectors[] = [
     {
       facetName: "AlchemicaFacet",
-      addSelectors: [
-        //"function channelAlchemica(uint256 _realmId,uint256 _gotchiId,uint256 _lastChanneled,bytes memory _signature)",
-      ],
+      addSelectors: [],
       removeSelectors: [],
     },
   ];
 
-  const ownership = (await ethers.getContractAt(
-    "OwnershipFacet",
-    maticInstallationDiamondAddress
-  )) as OwnershipFacet;
-  const owner = await ownership.owner();
-  console.log("owner:", owner);
+  const c = await varsForNetwork(ethers);
 
   const joined = convertFacetAndSelectorsToString(facets);
 
   const args: DeployUpgradeTaskArgs = {
     diamondUpgrader: diamondUpgrader,
-    diamondAddress: maticRealmDiamondAddress,
+    diamondAddress: c.realmDiamond,
     facetsAndAddSelectors: joined,
     useLedger: true,
     useMultisig: false,
