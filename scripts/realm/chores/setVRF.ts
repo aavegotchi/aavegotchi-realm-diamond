49f904dee9502ab46eabd72fc60e2f02054bb004
Coderdan <dmathieuva@gmail.com>
2022-07-07 11:29:09 +0800

    Update setVRF.ts

--- scripts/realm/chores/setVRF.ts
+++ scripts/realm/chores/setVRF.ts
@@ -1,56 +1,50 @@
-import { Signer } from "ethers";
-import { BigNumberish } from "@ethersproject/bignumber";
 import { ethers, network } from "hardhat";
-import { VRFFacet, OwnershipFacet } from "../../../typechain";
-import {
-  aavegotchiDAOAddress,
-  impersonate,
-  maticAavegotchiDiamondAddress,
-  maticDiamondAddress,
-  pixelcraftAddress,
-  gasPrice,
-} from "../../helperFunctions";
-import { alchemicaTotals, boostMultipliers } from "../../setVars";
+import { varsForNetwork } from "../../../constants";
+import { VRFFacet } from "../../../typechain";
 
 export async function setAddresses() {
-  const diamondAddress = "0x9351e6705590756BAc83f591aDE9f61De5998a84";
+  const c = await varsForNetwork(ethers);
 
   let vrfFacet = (await ethers.getContractAt(
     "VRFFacet",
-    diamondAddress
+    c.realmDiamond
   )) as VRFFacet;
 
-  console.log("subscribe");
-  const subTx = await vrfFacet.subscribe({
-    gasPrice: 500000000000,
-  });
+  // console.log("subscribe");
+  // const subTx = await vrfFacet.subscribe({
+  //   gasPrice: 500000000000,
+  // });
 
-  await subTx.wait();
+  // await subTx.wait();
 
-  console.log("topup");
-  const topTx = await vrfFacet.topUpSubscription(
-    ethers.utils.parseUnits("0.1"),
-    {
-      gasPrice: 500000000000,
-    }
-  );
+  // console.log("topup");
+  // const topTx = await vrfFacet.topUpSubscription(
+  //   ethers.utils.parseUnits("0.1"),
+  //   {
+  //     gasPrice: 500000000000,
+  //   }
+  // );
 
-  await topTx.wait();
+  // await topTx.wait();
 
   console.log("Setting vrf");
 
   const requestConfig = {
     subId: 0,
-    callbackGasLimit: 2000000,
+    callbackGasLimit: 10000000,
     requestConfirmations: 3,
     numWords: 4,
     keyHash:
       "0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f",
   };
 
-  let tx = await vrfFacet.setConfig(requestConfig, {
-    gasPrice: 500000000000,
-  });
+  let tx = await vrfFacet.setConfig(
+    requestConfig,
+    "0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed",
+    {
+      gasPrice: 500000000000,
+    }
+  );
 
   await tx.wait();
 }
