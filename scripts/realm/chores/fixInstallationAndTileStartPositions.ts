4c95ef25cce0f4a7354641774ed52ec4116723ad
Coderdan <dmathieuva@gmail.com>
2022-07-17 19:29:07 +0800

    Update fixInstallationAndTileStartPositions.ts

--- scripts/realm/chores/fixInstallationAndTileStartPositions.ts
+++ scripts/realm/chores/fixInstallationAndTileStartPositions.ts
@@ -1,6 +1,6 @@
-const apollo = require("apollo-fetch");
 const fs = require("fs").promises;
 const hre = require("hardhat");
+import { request } from "graphql-request";
 import { ethers, network } from "hardhat";
 import { Ownable, RealmGridFacet } from "../../../typechain";
 import { impersonate } from "../../helperFunctions";
@@ -13,239 +13,64 @@ let id = DEFAULT_BLOCKNUMBER;
 
 const uri =
   "https://api.thegraph.com/subgraphs/name/aavegotchi/gotchiverse-matic";
-const graph = apollo.createApolloFetch({
-  uri,
-});
 
-function getEquipInstallationsQuery() {
-  return `{
-    equipInstallationEvents(first: 1000 where: {id_gt: "${id}"} orderBy: id orderDirection: asc) {
+function getInstallationsQuery() {
+  return `
+    {installations(first: 1000 where: {id_gt:"${id}" equipped: true}) {
       id
-      block
       x
       y
-      realmId
-      installationId
-    }
-  }`;
-}
-
-function getUnequipInstallationsQuery() {
-  return `{
-    unequipInstallationEvents(first: 1000 where: {id_gt: "${id}"} orderBy: id orderDirection: asc) {
-      id
-      block
-      x
-      y
-      realmId
-      installationId
-    }
-  }`;
-}
-
-function getEquipTilesQuery() {
-  return `{
-    equipTileEvents(first: 1000 where: {id_gt: "${id}"} orderBy: id orderDirection: asc) {
-      id
-      block
-      x
-      y
-      realmId
-      tileId  
-    }
-  }`;
-}
-
-function getUnequipTilesQuery() {
-  return `{
-    unequipTileEvents(first: 1000 where: {id_gt: "${id}"} orderBy: id orderDirection: asc) {
-      id
-      block
-      x
-      y
-      realmId
-      tileId  
-    }
-  }`;
-}
-
-function getInstallationUpgradedQuery() {
-  return `{upgradeInitiatedEvents(first: 1000 where: {id_gt: "${id}"} orderBy: id orderDirection: asc) {
+      type {
         id
-        x
-        y
-        realmId
-        installationId
       }
-    }
-  `;
+      parcel {
+        id
+      }
+    }}`;
+}
+
+function getTilesQuery() {
+  return `
+    {tiles(first: 1000 skip: 0 where: {id_gt:"${id}" equipped: true}) {
+      id
+      x
+      y
+      type {
+        id
+      }
+      parcel {
+        id
+      }
+    }}`;
 }
 
 async function main() {
   await upgrade();
+
   // Tiles
-  // fetch first equip results
-  let equippedTiles = [];
-  let equipTilesDataTmp = (await graph({ query: getEquipTilesQuery() })).data
-    .equipTileEvents;
-  while (equipTilesDataTmp.length > 0) {
-    id = equipTilesDataTmp[equipTilesDataTmp.length - 1].id;
-    equippedTiles = equippedTiles.concat(equipTilesDataTmp);
-    equipTilesDataTmp = (await graph({ query: getEquipTilesQuery() })).data
-      .equipTileEvents;
+  let tiles = [];
+  let tilesTmp = (await request(uri, getTilesQuery())).tiles;
+  while (tilesTmp.length > 0) {
+    id = tilesTmp[tilesTmp.length - 1].id;
+    tiles = tiles.concat(tilesTmp);
+    tilesTmp = (await request(uri, getTilesQuery())).tiles;
 
-    console.log("Equipped Tiles: ", equippedTiles.length);
+    // console.log("Tiles: ", tiles.length);
   }
-
-  // fetch uneqipped
-  id = DEFAULT_BLOCKNUMBER;
-  let uneqippedTiles = [];
-  let unequipTilesDataTmp = (await graph({ query: getUnequipTilesQuery() }))
-    .data.unequipTileEvents;
-  while (unequipTilesDataTmp.length > 0) {
-    id = unequipTilesDataTmp[unequipTilesDataTmp.length - 1].id;
-    uneqippedTiles = uneqippedTiles.concat(unequipTilesDataTmp);
-    unequipTilesDataTmp = (await graph({ query: getUnequipTilesQuery() })).data
-      .unequipTileEvents;
-    console.log("Unequipped Tiles: ", uneqippedTiles.length);
-  }
-
-  interface FinalEquippedTiles {
-    x: BigNumberish;
-    y: BigNumberish;
-    realmId: BigNumberish;
-    tileId: BigNumberish;
-  }
-
-  // remove uneqipped from equipped
-  let finalEquippedTiles: FinalEquippedTiles[] = equippedTiles;
-  uneqippedTiles.forEach((e) => {
-    finalEquippedTiles = finalEquippedTiles.filter(
-      (f) =>
-        e.x != f.x ||
-        e.y != e.y ||
-        e.realmId != f.realmId ||
-        e.tileId != f.tileId
-    );
-  });
-
-  console.log("Final equipped Tiles: ", finalEquippedTiles.length);
 
   // Installations
-  // fetch first equip results
-  let equippedInstallations = [];
-  let equippedInstallationsTmp = (
-    await graph({ query: getEquipInstallationsQuery() })
-  ).data.equipInstallationEvents;
-
-  while (equippedInstallationsTmp.length > 0) {
-    id = equippedInstallationsTmp[equippedInstallationsTmp.length - 1].id;
-    equippedInstallations = equippedInstallations.concat(
-      equippedInstallationsTmp
-    );
-    equippedInstallationsTmp = (
-      await graph({ query: getEquipInstallationsQuery() })
-    ).data.equipInstallationEvents;
-
-    console.log("Equipped Installations: ", equippedInstallations.length);
-  }
-
-  // handle Upgrades
-  // fetch uneqipped
   id = DEFAULT_BLOCKNUMBER;
-  let installationUpgradedEvents = [];
-  let installationUpgradedEventsData = (
-    await graph({ query: getInstallationUpgradedQuery() })
-  ).data.upgradeInitiatedEvents;
-  while (installationUpgradedEventsData.length > 0) {
-    id =
-      installationUpgradedEventsData[installationUpgradedEventsData.length - 1]
-        .id;
-    installationUpgradedEvents = installationUpgradedEvents.concat(
-      installationUpgradedEventsData
-    );
-    installationUpgradedEventsData = (
-      await graph({ query: getInstallationUpgradedQuery() })
-    ).data.upgradeInitiatedEvents;
-    console.log("Upgraded Installations: ", installationUpgradedEvents.length);
+  let installations = [];
+  let installationsTmp = (await request(uri, getInstallationsQuery()))
+    .installations;
+
+  while (installationsTmp.length > 0) {
+    id = installationsTmp[installationsTmp.length - 1].id;
+    installations = installations.concat(installationsTmp);
+    installationsTmp = (await request(uri, getInstallationsQuery()))
+      .installations;
   }
 
-  equippedInstallations = equippedInstallations.map((e) => {
-    let upgrades = installationUpgradedEvents.filter(
-      (f) => f.realmId == e.realmId && f.x == e.x && f.y == e.y
-    );
-    if (upgrades.length == 0) {
-      return e;
-    }
-
-    let highestInstallationId = upgrades[0].installationId;
-    upgrades.forEach((u) => {
-      if (parseInt(u.installationId) > parseInt(highestInstallationId)) {
-        highestInstallationId = u.installationId;
-      }
-    });
-
-    return {
-      id: e.realmId + "-" + highestInstallationId,
-      x: e.x,
-      y: e.y,
-      realmId: e.realmId,
-      installationId: highestInstallationId,
-    };
-  });
-
-  // remove uneqipped from equipped
-  // fetch uneqipped
-  id = DEFAULT_BLOCKNUMBER;
-  let uneqippedInstallations = [];
-  let uneqippedInstallationsData = (
-    await graph({ query: getUnequipInstallationsQuery() })
-  ).data.unequipInstallationEvents;
-  while (uneqippedInstallationsData.length > 0) {
-    id = uneqippedInstallationsData[uneqippedInstallationsData.length - 1].id;
-    uneqippedInstallations = uneqippedInstallations.concat(
-      uneqippedInstallationsData
-    );
-    uneqippedInstallationsData = (
-      await graph({ query: getUnequipInstallationsQuery() })
-    ).data.unequipInstallationEvents;
-    console.log("Unequipped Installations: ", uneqippedInstallations.length);
-  }
-
-  interface FinalEquippedInstallations {
-    x: BigNumberish;
-    y: BigNumberish;
-    realmId: BigNumberish;
-    installationId: BigNumberish;
-  }
-
-  let finalEquippedInstallations: FinalEquippedInstallations[] =
-    equippedInstallations;
-  uneqippedInstallations.forEach((e) => {
-    finalEquippedInstallations = finalEquippedInstallations.filter(
-      (f) =>
-        e.x != f.x ||
-        e.y != e.y ||
-        e.realmId != f.realmId ||
-        e.installationId != f.installationId
-    );
-  });
-
-  console.log(
-    "Final equipped Installations: ",
-    finalEquippedInstallations.length
-  );
-
-  await fs.writeFile(
-    "./data/allInstallations.json",
-    JSON.stringify(finalEquippedInstallations),
-    "utf8"
-  );
-  await fs.writeFile(
-    "./data/allTiles.json",
-    JSON.stringify(finalEquippedTiles),
-    "utf8"
-  );
   const testing = ["hardhat", "localhost"].includes(hre.network.name);
 
   const accounts = await ethers.getSigners();
@@ -266,22 +91,32 @@ async function main() {
 
   if (testing) {
     realmFacet = await impersonate(owner, realmFacet, ethers, network);
+
+    console.log("tile length:", tiles.length);
+    console.log("installation length:", installations.length);
   }
 
   console.log("Fixing installation start positions");
-  for (let i = 0; i < finalEquippedInstallations.length / 100; i++) {
+  const batchSize = 500;
+  for (let i = 0; i < installations.length / batchSize; i++) {
     let realmIds: BigNumberish[] = [];
     let xs: BigNumberish[] = [];
     let ys: BigNumberish[] = [];
     let ids: BigNumberish[] = [];
-    for (let j = 0; j < 100; j++) {
-      const pos = i * 100 + j;
+    for (let j = 0; j < batchSize; j++) {
+      const pos = i * batchSize + j;
 
-      realmIds.push(finalEquippedInstallations[pos].realmId);
-      xs.push(finalEquippedInstallations[pos].x);
-      ys.push(finalEquippedInstallations[pos].y);
-      ids.push(finalEquippedInstallations[pos].installationId);
+      realmIds.push(installations[pos].parcel.id);
+      xs.push(installations[pos].x);
+      ys.push(installations[pos].y);
+      ids.push(installations[pos].type.id);
     }
+
+    console.log("realm ids:", realmIds);
+    console.log("xs", xs);
+    console.log("ys", ys);
+    console.log("ids:", ids);
+
     let tx = await realmFacet.fixGridStartPositions(
       realmIds,
       xs,
@@ -289,22 +124,23 @@ async function main() {
       false,
       ids
     );
-    console.log("TXID: ", tx.hash);
+
+    console.log("TXID: ", tx.hash, tx.gasLimit.toString());
     await tx.wait();
   }
 
   console.log("Fixing tile start positions");
-  for (let i = 0; i < finalEquippedTiles.length / 100; i++) {
+  for (let i = 0; i < tiles.length / batchSize; i++) {
     let realmIds: BigNumberish[] = [];
     let xs: BigNumberish[] = [];
     let ys: BigNumberish[] = [];
     let ids: BigNumberish[] = [];
-    for (let j = 0; j < 100; j++) {
-      const pos = i * 100 + j;
-      realmIds.push(finalEquippedTiles[pos].realmId);
-      xs.push(finalEquippedTiles[pos].x);
-      ys.push(finalEquippedTiles[pos].y);
-      ids.push(finalEquippedTiles[pos].tileId);
+    for (let j = 0; j < batchSize; j++) {
+      const pos = i * batchSize + j;
+      realmIds.push(tiles[pos].parcel.id);
+      xs.push(tiles[pos].x);
+      ys.push(tiles[pos].y);
+      ids.push(tiles[pos].type.id);
     }
     let tx = await realmFacet.fixGridStartPositions(
       realmIds,
