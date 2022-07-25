6dad71e523da42751497f92ee64121e3188c4226
Patrick <pakim249@berkeley.edu>
2022-07-25 12:31:58 -0400

    Update realm deploy script

--- scripts/realm/upgrades/upgrade-checkStartCoordinate.ts
+++ scripts/realm/upgrades/upgrade-checkStartCoordinate.ts
@@ -7,7 +7,7 @@ import {
 } from "../../../tasks/deployUpgrade";
 import { RealmGridFacet, RealmFacet } from "../../../typechain";
 
-export async function upgradeGettersAndSetters() {
+export async function upgradeRealm() {
   const c = await varsForNetwork(ethers);
 
   const diamondUpgrader = "0xa370f2ADd2A9Fba8759147995d6A0641F8d7C119";
@@ -18,6 +18,16 @@ export async function upgradeGettersAndSetters() {
       addSelectors: [
         "function verifyAccessRight(uint256 _realmId,uint256 _gotchiId,uint256 _actionRight,address _sender) external view",
       ],
+      removeSelectors: [],
+    },
+    {
+      facetName: "RealmFacet",
+      addSelectors: [],
+      removeSelectors: [],
+    },
+    {
+      facetName: "AlchemicaFacet",
+      addSelectors: [],
       removeSelectors: [],
     },
   ];
@@ -40,7 +50,7 @@ export async function upgradeGettersAndSetters() {
 }
 
 if (require.main === module) {
-  upgradeGettersAndSetters()
+  upgradeRealm()
     .then(() => process.exit(0))
     // .then(() => console.log('upgrade completed') /* process.exit(0) */)
     .catch((error) => {
