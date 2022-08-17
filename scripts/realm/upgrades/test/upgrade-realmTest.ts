d86b29f54a52d3978a1fdf693660c254746b9d31
Coderdan <dmathieuva@gmail.com>
2022-08-17 12:43:40 +0800

    remove testfacet in upgrade

--- scripts/realm/upgrades/test/upgrade-realmTest.ts
+++ scripts/realm/upgrades/test/upgrade-realmTest.ts
@@ -14,18 +14,25 @@ export async function upgradeRealmTest() {
   const BatchEquip = `tuple(uint256[] types, bool[] equip, uint256[] ids, uint256[] x, uint256[] y)`;
 
   const facets: FacetsAndAddSelectors[] = [
+    // {
+    //   facetName: "TestRealmFacet",
+    //   addSelectors: [
+    //     `function mockBatchEquip(uint256 _realmId,${BatchEquip} memory _equipParams) external`,
+    //     `function mockEquipInstallation(uint256 _realmId, uint256 _installationId, uint256 _x, uint256 _y) public`,
+    //     `function mockUnequipInstallation(uint256 _realmId, uint256 _installationId, uint256 _x, uint256 _y) public`,
+    //     `function mockEquipTile(uint256 _realmId, uint256 _tileId, uint256 _x, uint256 _y) public`,
+    //     `function mockUnequipTile(uint256 _realmId, uint256 _tileId, uint256 _x, uint256 _y) public`,
+    //     `function mockStartSurveying(uint256 _realmId) external`,
+    //     `function mockRawFulfillRandomWords(uint256 tokenId, uint256 surveyingRound, uint256 seed) external`,
+    //     `function mockClaimAvailableAlchemica(uint256 _realmId, uint256 _gotchiId) external`,
+    //     `function mockMintParcels(address[] calldata _to, uint256[] calldata _tokenIds, ${MintParcelInput}[] memory _metadata) external`,
+    //   ],
+    //   removeSelectors: [],
+    // },
     {
-      facetName: "TestRealmFacet",
+      facetName: "RealmFacet",
       addSelectors: [
-        `function mockBatchEquip(uint256 _realmId,${BatchEquip} memory _equipParams) external`,
-        `function mockEquipInstallation(uint256 _realmId, uint256 _installationId, uint256 _x, uint256 _y) public`,
-        `function mockUnequipInstallation(uint256 _realmId, uint256 _installationId, uint256 _x, uint256 _y) public`,
-        `function mockEquipTile(uint256 _realmId, uint256 _tileId, uint256 _x, uint256 _y) public`,
-        `function mockUnequipTile(uint256 _realmId, uint256 _tileId, uint256 _x, uint256 _y) public`,
-        `function mockStartSurveying(uint256 _realmId) external`,
-        `function mockRawFulfillRandomWords(uint256 tokenId, uint256 surveyingRound, uint256 seed) external`,
-        `function mockClaimAvailableAlchemica(uint256 _realmId, uint256 _gotchiId) external`,
-        `function mockMintParcels(address[] calldata _to, uint256[] calldata _tokenIds, ${MintParcelInput}[] memory _metadata) external`,
+        `function batchEquip(uint256 _realmId,uint256 _gotchiId,${BatchEquip} memory _params,bytes[] memory _signatures) external`,
       ],
       removeSelectors: [],
     },
