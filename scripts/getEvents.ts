61e6ce65adaea0cad22c923b0647784b22a56906
Timidan <akinnusotutemitayodaniel@gmail.com>
2022-12-17 03:44:44 +0100

    update addresses

--- scripts/getEvents.ts
+++ scripts/getEvents.ts
@@ -8,28 +8,39 @@ const path = "scripts/xmasInstallationEvents.json";
 const client = new GraphQLClient(graphqlClientUrl);
 
 const startBlock = "26540483";
-const endBlock = "36902871";
+const endBlock = "36922476";
 
 const getCraftEvents = () => {
   return `{
     first: mintInstallationEvents(first: 1000, skip: 0, where:{installationType_in: ["152", "153","154","155"], block_gte:${startBlock} ,block_lte:${endBlock}}) {
       owner
+      quantity
     }
     second: mintInstallationEvents(first: 1000, skip: 1000, where:{installationType_in: ["152", "153","154","155"],  block_gte:${startBlock} ,block_lte:${endBlock}}) {
       owner
+      quantity
     }
     third: mintInstallationEvents(first: 1000, skip: 2000, where:{installationType_in: ["152", "153","154","155"],  block_gte:${startBlock} ,block_lte:${endBlock}}) {
       owner
+      quantity
     }
     fourth: mintInstallationEvents(first: 1000, skip: 3000, where:{installationType_in: ["152", "153","154","155"], block_gte:${startBlock} ,block_lte:${endBlock}}) {
       owner
+      quantity
     }
     fifth: mintInstallationEvents(first: 1000, skip: 4000, where:{installationType_in: ["152", "153","154","155"], block_gte:${startBlock} ,block_lte:${endBlock}}) {
       owner
+      quantity
     }
   }
     `;
 };
+
+function logInto(count, data, arr) {
+  for (let i = 0; i < count; i++) {
+    arr.push(data);
+  }
+}
 
 const getQuery = async (queryString: String) => {
   const query = gql`
@@ -40,7 +51,8 @@ const getQuery = async (queryString: String) => {
   for (const key in data) {
     if (data.hasOwnProperty(key)) {
       for (const element of data[key]) {
-        addresses.push(element.owner);
+        const count = element.quantity;
+        logInto(count, element.owner, addresses);
       }
     }
   }
