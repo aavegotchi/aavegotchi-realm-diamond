cff4a9af25269fc7d124cea286273aa172871dc7
Vinicius Macelai <vinicius.macelai@gmail.com>
2023-06-28 13:15:07 -0300

    fix: remove unecessary code and fix style

--- contracts/bridge/InstallationsBridgeGotchichainSide.sol
+++ contracts/bridge/InstallationsBridgeGotchichainSide.sol
@@ -5,25 +5,13 @@ import "./ProxyONFT1155.sol";
 import "../InstallationDiamond/facets/PolygonXGotchichainBridgeFacet.sol";
 
 contract InstallationsBridgeGotchichainSide is ProxyONFT1155 {
-    constructor(address _lzEndpoint, address _proxyToken) ProxyONFT1155(_lzEndpoint, _proxyToken) {}
+  constructor(address _lzEndpoint, address _proxyToken) ProxyONFT1155(_lzEndpoint, _proxyToken) {}
 
-    function estimateSendBatchFee(
-        uint16 _dstChainId,
-        bytes memory _toAddress,
-        uint[] memory _tokenIds,
-        uint[] memory _amounts,
-        bool _useZro,
-        bytes memory _adapterParams
-    ) public view override returns (uint nativeFee, uint zroFee) {
-        bytes memory payload = abi.encode(_toAddress, _tokenIds, _amounts);
-        return lzEndpoint.estimateFees(_dstChainId, address(this), payload, _useZro, _adapterParams);
-    }
+  function _debitFrom(address _from, uint16, bytes memory, uint[] memory _tokenIds, uint[] memory _amounts) internal override {
+    revert("InstallationsBridgeGotchichainSide: not able to bridge it back");
+  }
 
-    function _debitFrom(address _from, uint16, bytes memory, uint[] memory _tokenIds, uint[] memory _amounts) internal override {
-       revert("InstallationsBridgeGotchichainSide: not able to bridge it back");
-    }
-
-    function _creditTo(uint16, address _toAddress, uint[] memory _tokenIds, uint[] memory _amounts) internal override {
-        InstallationsPolygonXGotchichainBridgeFacet(address(token)).addItemsToOwner(_toAddress, _tokenIds, _amounts);
-    }
+  function _creditTo(uint16, address _toAddress, uint[] memory _tokenIds, uint[] memory _amounts) internal override {
+    InstallationsPolygonXGotchichainBridgeFacet(address(token)).addItemsToOwner(_toAddress, _tokenIds, _amounts);
+  }
 }
