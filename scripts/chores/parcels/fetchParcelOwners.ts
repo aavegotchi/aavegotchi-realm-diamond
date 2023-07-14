dd7a878dccf540361f078b1222bdffc053175c81
orionstardust <justinono1990@outlook.com>
2023-07-14 05:42:45 -0500

    Fix tests

--- scripts/chores/parcels/fetchParcelOwners.ts
+++ scripts/chores/parcels/fetchParcelOwners.ts
@@ -1,45 +1,66 @@
-import request from "graphql-request";
+const apollo = require("apollo-fetch");
+const uri =
+  "https://subgraph.satsuma-prod.com/tWYl5n5y04oz/aavegotchi/gotchiverse-matic/api";
+// "https://subgraph.satsuma-prod.com/tWYl5n5y04oz/aavegotchi/gotchiverse-mumbai/api";
+const graph = apollo.createApolloFetch({
+  uri,
+});
 
-export async function getParcels() {
-  const gotchiverseSubgraph =
-    "https://api.thegraph.com/subgraphs/name/aavegotchi/gotchiverse-matic";
+const fs = require("fs").promises;
+
+async function getParcels() {
   const currentBlock = 45013446;
 
-  const query = `
-    {
-      parcels(block: {number: ${currentBlock}}) {
-        id
-        owner
-        tokenId
-        parcelId
-        parcelHash
-        coordinateX
-        coordinateY
-        district
-        alphaBoost
-        fomoBoost
-        fudBoost
-        kekBoost
-        lastChanneledAlchemica
-        remainingAlchemica
-        lastClaimedAlchemica
-        surveyRound
-        size
-        equippedInstallations {
-          id
-          width
-          height
+  let allParcels = [];
+  let id = 0;
+  let parcels = [];
+  do {
+    let query = `
+        {
+          parcels(first: 2500  where:{ id_gt: ${id}} orderBy: id orderDirection: asc block: {number: ${currentBlock}}) {
+            id
+            owner
+            tokenId
+            parcelId
+            parcelHash
+            coordinateX
+            coordinateY
+            district
+            alphaBoost
+            fomoBoost
+            fudBoost
+            kekBoost
+            lastChanneledAlchemica
+            remainingAlchemica
+            lastClaimedAlchemica
+            surveyRound
+            size
+            equippedInstallations {
+              id
+              width
+              height
+            }
+            equippedTiles {
+              id
+              height
+              width
+            }
+          }
         }
-        equippedTiles {
-          id
-          height
-          width
-        }
-      }
+      `;
+
+    let response = await graph({ query });
+    parcels = response.data.parcels;
+
+    if (parcels.length > 0) {
+      allParcels = allParcels.concat(parcels);
+      id = parcels[parcels.length - 1].id;
     }
-  `;
-  const parcelData = await request(gotchiverseSubgraph, query);
-  console.log("parcelData", parcelData);
+  } while (parcels.length > 0);
+  console.log(allParcels.length);
+
+  const json = JSON.stringify(allParcels);
+  await fs.writeFile("./allParcels.json", json, "utf8");
 }
 
 // We recommend this pattern to be able to use async/await everywhere
