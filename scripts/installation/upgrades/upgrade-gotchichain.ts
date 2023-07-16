5630b0334235aaefd6af127d5f3cf927db6549e1
Narayan Prusty <arsenal.narayan@gmail.com>
2023-07-16 14:52:34 +0100

    added script to upgrade installation

---
+++ scripts/installation/upgrades/upgrade-gotchichain.ts
@@ -0,0 +1,64 @@
+import { ethers, run } from "hardhat";
+import { varsForNetwork } from "../../constants";
+import { DeployUpgradeTaskArgs, FacetsAndAddSelectors, convertFacetAndSelectorsToString } from "../../tasks/deployUpgrade";
+import { InstallationFacetInterface } from "../../typechain-types/contracts/InstallationDiamond/facets/InstallationFacet";
+import { InstallationFacet__factory } from "../../typechain-types";
+
+const gotchichainBridgeAddress = "0xe6517944BbA81e86Bf619DF6D5b549348Bb70446";
+
+export async function upgradeInstallation(bridgeAddress: string) {
+  const diamondUpgrader = "0x296903b6049161bebEc75F6f391a930bdDBDbbFc";
+
+  const facets: FacetsAndAddSelectors[] = [
+    {
+      facetName: "InstallationFacet",
+      addSelectors: [
+        "function addLayerZeroBridgeAddress(address _newLayerZeroBridge) external",
+        "function removeLayerZeroBridgeAddress(address _layerZeroBridgeToRemove) external"
+      ],
+      removeSelectors: [],
+    },
+    {
+      facetName: "InstallationsPolygonXGotchichainBridgeFacet",
+      addSelectors: [
+        "function removeItemsFromOwner(address _owner, uint256[] calldata _tokenIds, uint256[] calldata _tokenAmounts) external",
+        "function addItemsToOwner(address _owner, uint256[] calldata _tokenIds, uint256[] calldata _tokenAmounts) external"
+      ],
+      removeSelectors: [],
+    },
+  ];
+
+  const c = await varsForNetwork(ethers);
+
+  const joined = convertFacetAndSelectorsToString(facets);
+
+  let iface = new ethers.utils.Interface(
+    InstallationFacet__factory.abi
+  );
+
+  const calldata = iface.encodeFunctionData("addLayerZeroBridgeAddress", [
+    bridgeAddress,
+  ]);
+
+  const args: DeployUpgradeTaskArgs = {
+    diamondUpgrader: diamondUpgrader,
+    diamondAddress: c.installationDiamond,
+    facetsAndAddSelectors: joined,
+    useLedger: false,
+    useMultisig: false,
+    initAddress: c.installationDiamond,
+    initCalldata: calldata,
+  };
+
+  await run("deployUpgrade", args);
+}
+
+if (require.main === module) {
+  upgradeInstallation(gotchichainBridgeAddress)
+    .then(() => process.exit(0))
+    // .then(() => console.log('upgrade completed') /* process.exit(0) */)
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
