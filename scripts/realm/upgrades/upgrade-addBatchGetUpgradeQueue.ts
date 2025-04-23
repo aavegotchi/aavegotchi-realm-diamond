ee3daa769fc13a121c214bd4483274d26bf7deab
Timidan <akinnusotutemitayodaniel@gmail.com>
2025-04-23 04:01:31 +0100

    feat: add batchGetParcelUpgradeQueueLength function for retrieving multiple parcel upgrade queue lengths

---
+++ scripts/realm/upgrades/upgrade-addBatchGetUpgradeQueue.ts
@@ -0,0 +1,54 @@
+import { run, ethers } from "hardhat";
+//import { maticTileDiamondAddress } from "../../../constants";
+import {
+  convertFacetAndSelectorsToString,
+  DeployUpgradeTaskArgs,
+  FacetsAndAddSelectors,
+} from "../../../tasks/deployUpgrade";
+import { OwnershipFacet } from "../../../typechain-types";
+import { varsForNetwork } from "../../../constants";
+
+export async function upgrade() {
+  const c = await varsForNetwork(ethers);
+
+  const facets: FacetsAndAddSelectors[] = [
+    {
+      facetName: "RealmGettersAndSettersFacet",
+      addSelectors: [
+        "function batchGetParcelUpgradeQueueLength(uint256[] calldata _parcelIds) external view returns (uint256[] memory output_)",
+      ],
+      removeSelectors: [],
+    },
+  ];
+
+  const ownership = (await ethers.getContractAt(
+    "OwnershipFacet",
+    c.realmDiamond
+  )) as OwnershipFacet;
+
+  const owner = await ownership.owner();
+  console.log("owner:", owner);
+
+  const joined = convertFacetAndSelectorsToString(facets);
+
+  const args: DeployUpgradeTaskArgs = {
+    diamondAddress: c.realmDiamond,
+    facetsAndAddSelectors: joined,
+    useLedger: false,
+    useMultisig: false,
+    initAddress: ethers.constants.AddressZero,
+    initCalldata: "0x",
+  };
+
+  await run("deployUpgrade", args);
+}
+
+if (require.main === module) {
+  upgrade()
+    .then(() => process.exit(0))
+    // .then(() => console.log('upgrade completed') /* process.exit(0) */)
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
