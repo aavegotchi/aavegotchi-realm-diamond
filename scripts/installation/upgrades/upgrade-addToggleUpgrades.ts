e03ea792da596bdb3c554656423f6bae537af947
Timidan <akinnusotutemitayodaniel@gmail.com>
2025-04-28 23:53:24 +0100

    add upgradePaused

---
+++ scripts/installation/upgrades/upgrade-addToggleUpgrades.ts
@@ -0,0 +1,47 @@
+import { ethers, run } from "hardhat";
+import { varsForNetwork } from "../../../constants";
+import {
+  convertFacetAndSelectorsToString,
+  DeployUpgradeTaskArgs,
+  FacetsAndAddSelectors,
+} from "../../../tasks/deployUpgrade";
+
+export async function upgrade() {
+  const facets: FacetsAndAddSelectors[] = [
+    {
+      facetName: "InstallationUpgradeFacet",
+      addSelectors: [],
+      removeSelectors: [],
+    },
+    {
+      facetName: "InstallationAdminFacet",
+      addSelectors: ["function toggleUpgradePaused()"],
+      removeSelectors: [],
+    },
+  ];
+
+  const joined = convertFacetAndSelectorsToString(facets);
+
+  const c = await varsForNetwork(ethers);
+
+  const args: DeployUpgradeTaskArgs = {
+    diamondAddress: c.installationDiamond,
+    facetsAndAddSelectors: joined,
+    useLedger: true,
+    useMultisig: false,
+    initAddress: ethers.constants.AddressZero,
+    initCalldata: "0x",
+  };
+
+  await run("deployUpgrade", args);
+}
+
+if (require.main === module) {
+  upgrade()
+    .then(() => process.exit(0))
+    // .then(() => console.log('upgrade completed') /* process.exit(0) */)
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
