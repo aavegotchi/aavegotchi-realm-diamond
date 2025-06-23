9fb06becf27a180845d8764d08824fdb1619f0d6
Timidan <akinnusotutemitayodaniel@gmail.com>
2025-06-23 12:44:28 +0100

    fix

--- scripts/bridge/paths.ts
+++ scripts/bridge/paths.ts
@@ -2,3 +2,37 @@ export const DATA_DIR = `${__dirname}/cloneData`;
 export const DATA_DIR_PARCEL = `${DATA_DIR}/parcels`;
 export const DATA_DIR_INSTALLATIONS = `${DATA_DIR}/installations`;
 export const DATA_DIR_TILES = `${DATA_DIR}/tiles`;
+export const BLOCKNUMBERFILE = `${DATA_DIR}/blockNumber.json`;
+
+import fs from "fs";
+
+interface BlockNumber {
+  parcel: number;
+  installation: number;
+  tile: number;
+  parcelMetadata: number;
+}
+
+export async function writeBlockNumber(
+  assetType: "parcel" | "installation" | "tile" | "parcelMetadata",
+  ethers: any
+) {
+  const blockNumber = await ethers.provider.getBlockNumber();
+  console.log(
+    `Using anchor Block number for ${assetType} data: ${blockNumber}`
+  );
+  //create the file if it doesn't exist
+  if (!fs.existsSync(BLOCKNUMBERFILE)) {
+    fs.writeFileSync(
+      BLOCKNUMBERFILE,
+      JSON.stringify({ [assetType]: blockNumber }, null, 2)
+    );
+  }
+  //read the file and only update the assetType block number
+  const blockNumberObject: BlockNumber = JSON.parse(
+    fs.readFileSync(BLOCKNUMBERFILE, "utf8")
+  );
+  //direct update the block number
+  blockNumberObject[assetType] = blockNumber;
+  fs.writeFileSync(BLOCKNUMBERFILE, JSON.stringify(blockNumberObject, null, 2));
+}
